08eab074adb4dc6de98985800e41e0b292cc4428
Melina Boedecker <melinamboedecker@gmail.com>
2021-08-27 11:11:02 -0700

    added typeDefs and index and removed routes from server.js

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,39 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
+    bookCount: Int
+  }
+
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookL BookData!): User
+    removeBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
